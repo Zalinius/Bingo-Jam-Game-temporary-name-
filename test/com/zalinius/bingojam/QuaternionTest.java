53c492914dd1ca9d9f341b690ec89b2dc306ec9d
Zachary Lapointe <lapointez@hotmail.ca>
2021-06-26 14:57:06 -0400

    renamed file

--- test/com/zalinius/bingojam/QuaternionTest.java
+++ test/com/zalinius/bingojam/QuaternionTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class QuaternionTests {
+public class QuaternionTest {
 	public final double ERROR_MARGIN = 1E-12;
 	
 	@ParameterizedTest
